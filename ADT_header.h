ff1537be3eaf687cb39593073ace68a5e9165525
shiyaliii2 <fullidoangelho6@gmail.com>
2025-05-04 09:16:42 +0800

    ADT_header.h

---
+++ ADT_header.h
@@ -0,0 +1,31 @@
+#ifndef FADT_HEADERS_H
+#define FADT_HEADERS_H
+#include <vector>
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+class Assignment {
+private:
+    string subject;
+    string name;
+    time_t dueDate;
+public:
+    Assignment(string sub, string nm, time_t due);
+    string getSubject() const;
+    string getName() const;
+    time_t getDueDate() const;
+    void display() const;
+};
+
+time_t createDueDate(int year, int month, int day);
+string formatDate(time_t date);
+void checkReminders(const vector<Assignment>& assignments);
+void PrintMenu();
+
+// New file I/O functions
+void saveAssignments(const vector<Assignment>& assignments, const string& filename);
+vector<Assignment> loadAssignments(const string& filename);
+
+#endif // FADT_HEADERS_H
